aedcd8d4336054b72640fcf7fab640450e45b99d
Ali Castillo <alicastilo1097@gmail.com>
2024-07-06 07:38:01 +0300

    create: Client and Super rare components

--- src/Components/Clients.jsx
+++ src/Components/Clients.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import clients1 from "../assets/clients1.png";
+import clients2 from "../assets/clients2.png";
+import clients3 from "../assets/clients3.png";
+import clients4 from "../assets/clients4.png";
+import clients5 from "../assets/clients5.png";
 
 const Clients = () => {
+  let data = [clients1, clients2, clients3, clients4, clients5];
   return (
-    <div>
-      
+    <div className="client">
+      <div className="container">
+        <div className="clients-container">
+          {data.map((client, i) => (
+            <div className="client" key={i}>
+              <img src={client} alt="Client" className="client" />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Clients
+export default Clients;
--- src/Components/Release.jsx
+++ src/Components/Release.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 
 const Release = () => {
   return (
-    <div>
-      
+    <div className="releases">
+      <div className="release orange">
+        <div className="content">
+          <h2 className="title">Initial Release 4/11</h2>
+          <p className="description">
+            We have released four limited edition NFTs early which can be bid on
+            via <a href="#">OpenSea</a>
+          </p>
+        </div>
+      </div>
+      <div className="release green"></div>
     </div>
-  )
+  );
 }
 
 export default Release
